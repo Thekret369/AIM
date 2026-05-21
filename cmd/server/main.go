d0b14e8457ca2df78ac9816dd6736862902a6d03
Thekret <965659585@qq.com>
2026-05-21 19:45:57 +0800

    暴露消息撤回接口

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -229,6 +229,7 @@ func main() {
 
 		// 消息历史
 		auth.GET("/search/messages", chatH.SearchMessages)
+		auth.POST("/messages/:id/recall", chatH.RecallMessage)
 		auth.GET("/history", chatH.GetHistory)
 		auth.GET("/history/group/:id", chatH.GetGroupHistory)
 		auth.GET("/history/broadcast", chatH.GetBroadcastHistory)
